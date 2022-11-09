2812b3f8d03d1cb7446c16e9aaeca97853af35e5
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-09 17:39:58 +0100

    Create the user model

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
