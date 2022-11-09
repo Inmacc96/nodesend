cb5e8112fab786524bb1cad668c00dd68d69360a
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-09 19:34:37 +0100

    Define the controller to get the authenticated user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,4 +41,21 @@ exports.authenticateUser = async (req, res, next) => {
   }
 };
 
-exports.authenticatedUser = (req, res) => {};
+exports.authenticatedUser = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+
+  if (authHeader) {
+    // Obtener el Token
+    const token = authHeader.split(" ")[1];
+
+    // Comprobar el JWT
+    try {
+      const user = jwt.verify(token, process.env.JWT_SECRET);
+      res.json({ user });
+    } catch (err) {
+      const error = new Error("Token invalid");
+      return res.status(401).json({ msg: error.message });
+    }
+  }
+  return next();
+};
