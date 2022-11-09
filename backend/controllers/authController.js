304d1abd296f3da0316b27e8aac63fde370d5432
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-09 18:58:09 +0100

    Check that user exists

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,19 @@
-exports.authenticateUser = (req, res, next) => {};
+const User = require("../models/User");
+
+exports.authenticateUser = async (req, res, next) => {
+  // Revisar si hay errores
+  // Buscar el usuario para ver si está registrado
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    const error = new Error("The user doesn't exist");
+    res.status(401).json({ msg: error.message });
+    return next();
+  }
+
+  // Verificar el password y autenticar el usuario
+  
+};
 
 exports.authenticatedUser = (req, res) => {};
