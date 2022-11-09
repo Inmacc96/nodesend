ecad902c5323e03ac4203be7c6bda01b637a90c5
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-09 21:01:12 +0100

    Add validation to createLink input data

--- backend/controllers/linksController.js
+++ backend/controllers/linksController.js
@@ -1,21 +1,47 @@
 const Links = require("../models/Links");
 const shortid = require("shortid");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 
 exports.createLink = async (req, res, next) => {
   // Revisar si hay errores
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
   // Crear un objeto de Enlace
-  const { name_original, password } = req.body;
+  const { name_original } = req.body;
 
   const link = new Links();
   link.url = shortid.generate();
   link.name = shortid.generate();
   link.name_original = name_original;
-  link.password = password;
+
+  // Si el usuario está autenticado
+
+  if (req.user) {
+    const { password, downloads } = req.body;
+
+    //Asignar a enlace el nº de descargas
+    if (downloads) {
+      link.downloads = downloads;
+    }
+
+    // Asignar un password
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      link.password = await bcrypt.hash(password, salt);
+    }
+
+    // Asignar el autor
+    link.author = req.user.id;
+  }
 
   // Almacenar el enlace en la base de datos
   try {
     await link.save();
+    console.log(link);
     res.json({ msg: `${link.url}` });
     return next();
   } catch (err) {
--- backend/routes/linksRoutes.js
+++ backend/routes/linksRoutes.js
@@ -4,6 +4,14 @@ const linksController = require("../controllers/linksController");
 const { check } = require("express-validator");
 const auth = require("../middleware/auth");
 
-router.post("/", auth, linksController.createLink);
+router.post(
+  "/",
+  [
+    check("name", "Upload a file").not().isEmpty(),
+    check("name_original", "Upload a file").not().isEmpty(),
+  ],
+  auth,
+  linksController.createLink
+);
 
 module.exports = router;
