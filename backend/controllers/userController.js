6c9c6c7879156270948639450979a8dd596f35f7
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-09 17:57:47 +0100

    Check that the user has not been registered before

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,22 @@
 const User = require("../models/User");
 
 exports.createUser = async (req, res) => {
-  console.log(req.body);
+  // Verificar si el usuario existe
+  const { email } = req.body;
 
-  const user = new User(req.body);
-  await user.save();
+  const userExists = await User.findOne({ email });
 
-  res.json({ msg: "User successfully created" });
+  if (userExists) {
+    const error = new Error("Already registered user");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  try {
+    const user = new User(req.body);
+    await user.save();
+
+    res.json({ msg: "User successfully created" });
+  } catch (err) {
+    console.log(err);
+  }
 };
