db8e414455c8df2cf84c9d534890c2e3cf831c52
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-09 19:15:20 +0100

    Generate JWT when password is correct

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: ".env" });
 
 exports.authenticateUser = async (req, res, next) => {
   // Revisar si hay errores
@@ -16,6 +18,17 @@ exports.authenticateUser = async (req, res, next) => {
   // Verificar el password y autenticar el usuario
   if (bcrypt.compareSync(password, user.password)) {
     //Crear JWT
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "8h" }
+    );
+
+    res.json({ token });
   } else {
     const error = new Error("The password is incorrect");
     res.status(401).json({ msg: error.message });
