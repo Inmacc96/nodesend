d4a0e7a8764291cb4361a7939abbf98a8158c1a4
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-12 17:01:53 +0100

    Create the state for context

--- frontend/context/auth/authReducer.js
+++ frontend/context/auth/authReducer.js
@@ -1,1 +1,9 @@
 // Va a contener las funciones que modifica al state
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
--- frontend/context/auth/authState.js
+++ frontend/context/auth/authState.js
@@ -1,9 +1,34 @@
 // Contiene las acciones que dispara lo que tenemos en el reducer
 import { useReducer } from "react";
 import authContext from "./authContext";
+import authReducer from "./authReducer";
 
 const AuthState = ({ children }) => {
-  return <authContext.Provider value={{}}>{children}</authContext.Provider>;
+  //Definir un state inicial
+  const initialState = {
+    token: "",
+    isAuth: null,
+    user: null,
+    message: null,
+  };
+
+  // Definir el reducer
+  // La función dispatch manda a llamar las funciones definidas en el reducer
+  // useReducer recibe la función reducer y el state inicial
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  return (
+    <authContext.Provider
+      value={{
+        token: state.token,
+        isAuth: state.isAuth,
+        user: state.user,
+        message: state.message,
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
 };
 
 export default AuthState;
