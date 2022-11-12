6cc84105baf5d83cc582bd1ba6907194908f52bc
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-12 19:56:57 +0100

    Design home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import authContext from "../context/auth/authContext";
+import Link from "next/link";
 
 const Home = () => {
   // Extraer el usuario autenticado del storage
@@ -8,7 +9,32 @@ const Home = () => {
   useEffect(() => {
     getAuthenticatedUser();
   }, []);
-  return <h1>Index</h1>;
+  return (
+    <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+      <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+        <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+          <p>Dropzone aqui</p>
+        </div>
+
+        <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+          <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
+            Simple and private file sharing
+          </h2>
+          <p className="text-lg leading-loose">
+            <span className="text-red-500 font-bold">ReactNodeSend</span> {" "}
+            allows you to share files with end-to-end encryption and a file is
+            deleted after it is downloaded. So you can keep what you share
+            private and make sure your stuff does not stay online forever.
+          </p>
+          <Link href="/signup">
+            <p className="text-red-500 font-bold text-lg hover:text-red-700 hover:cursor-pointer">
+            Create an account for more benefits
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
