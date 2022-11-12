7d18e57eb24a2779172d6e806b48f5e3fc2c95ad
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-12 17:27:58 +0100

    Create an axios client

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    backendURL: "http://localhost:4000/api/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
