de9bc5df62cf1862371940191af74552e9aa9139
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-12 21:29:39 +0100

    Show the Create link button after uploading the file

--- frontend/components/Dropzone.jsx
+++ frontend/components/Dropzone.jsx
@@ -24,43 +24,58 @@ const Dropzone = () => {
   const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
     useDropzone({ onDrop });
 
+  const createLink = () => {};
+
   return (
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col justify-center items-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-      <ul>
-        {acceptedFiles.map((file) => (
-          <li
-            key={file.lastModified}
-            className="bg-white flex-1 p-3 mb-4 shadow-lg rounded"
+      {acceptedFiles.length > 0 ? (
+        <div className="mt-10 w-full">
+          <h4 className="text-2xl font-bold text-center mb-4">Files</h4>
+          <ul>
+            {acceptedFiles.map((file) => (
+              <li
+                key={file.lastModified}
+                className="bg-white flex-1 p-3 mb-4 shadow-lg rounded"
+              >
+                <p className="font-bold text-xl">{file.path}</p>
+                <p className="text-sm text-gray-500">
+                  {(file.size / Math.pow(1024, 2)).toFixed(2)} MB
+                </p>
+              </li>
+            ))}
+          </ul>
+
+          <button
+            type="button"
+            className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+            onClick={createLink}
           >
-            <p className="font-bold text-xl">{file.path}</p>
-            <p className="text-sm text-gray-500">
-              {(file.size / Math.pow(1024, 2)).toFixed(2)} MB
-            </p>
-          </li>
-        ))}
-      </ul>
+            Create link
+          </button>
+        </div>
+      ) : (
+        <div {...getRootProps({ className: "dropzone w-full py-32" })}>
+          <input className="h-100" {...getInputProps()} />
 
-      <div {...getRootProps({ className: "dropzone w-full py-32" })}>
-        <input className="h-100" {...getInputProps()} />
-
-        {isDragActive ? (
-          <p className="text-2xl text-center text-gray-600">
-            Drop the files here{" "}
-          </p>
-        ) : (
-          <div className="text-center">
+          {isDragActive ? (
             <p className="text-2xl text-center text-gray-600">
-              Drag and drop some files here, or click to select file
+              Drop the files here{" "}
             </p>
-            <button
-              className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
-              type="button"
-            >
-              Select files to upload
-            </button>
-          </div>
-        )}
-      </div>
+          ) : (
+            <div className="text-center">
+              <p className="text-2xl text-center text-gray-600">
+                Drag and drop some files here, or click to select file
+              </p>
+              <button
+                className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+                type="button"
+              >
+                Select files to upload
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
