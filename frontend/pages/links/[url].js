9f5af8d4557b1f71db5b95fa6b0ba5debe6d3a5d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-14 21:00:35 +0100

    Generate all urls of the page with getStaticPath and get the file corresponding to the link with getStaticProps

--- frontend/pages/links/[url].js
+++ frontend/pages/links/[url].js
@@ -1,15 +1,47 @@
+import clientAxios from "../../config/axios";
+
 // Los props estáticos van a ser la respuesta que vamos a obtener.
 //Ejemplo: Visito una url y obtenemos el registro de ese link de la bd
 export async function getStaticProps() {
-
+  try {
+    const { data } = await clientAxios("/links/HZVmt9-9D");
+    return {
+      props: {
+        link: data,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return { props: {} };
+  }
 }
 
 // Static Path es el routing
+// Va a generar todas las urls de todos los links de la db
 export async function getStaticPaths() {
+  try {
+    const { data } = await clientAxios("/links");
+    return {
+      paths: data.links.map((link) => ({
+        params: { url: link.url },
+      })),
+      fallback: false,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      paths: [],
+      fallback: false,
+    };
+  }
 
+  // Debes devolver un path: string de url o array de urls
+  // Si fallback es true y una página no existe va a mostrar algo, si es false
+  // muestra un 404
 }
 
-const Links = () => {
+const Links = ({ link }) => {
+  console.log(link);
   return <div>Links</div>;
 };
 
