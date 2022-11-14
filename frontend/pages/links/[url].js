9eac373652e34580fb9de2c13dfe9817ba056edb
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-14 17:30:56 +0100

    Add dynamic routes for links

---
+++ frontend/pages/links/[url].js
@@ -0,0 +1,16 @@
+// Los props estáticos van a ser la respuesta que vamos a obtener.
+//Ejemplo: Visito una url y obtenemos el registro de ese link de la bd
+export async function getStaticProps() {
+
+}
+
+// Static Path es el routing
+export async function getStaticPaths() {
+
+}
+
+const Links = () => {
+  return <div>Links</div>;
+};
+
+export default Links;
