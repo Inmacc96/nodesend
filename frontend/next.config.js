610f61f5f26da1d38a5c58b223138785255a0031
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-14 14:00:19 +0100

    - Show the url to share the file and download it - Add a button to copy that url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   env: {
     backendURL: "http://localhost:4000/api/",
+    frontendURL: "http://localhost:3000",
   },
 };
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
   const { getAuthenticatedUser } = useContext(authContext);
 
   // Extraer el mensaje de error de la subida
-  const { msg_file } = useContext(uploadContext);
+  const { msg_file, url } = useContext(uploadContext);
 
   useEffect(() => {
     getAuthenticatedUser();
@@ -18,28 +18,47 @@ const Home = () => {
 
   return (
     <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
-      {msg_file && <Alert message={msg_file} />}
-
-      <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-        <Dropzone />
-
-        <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
-          <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
-            Simple and private file sharing
-          </h2>
-          <p className="text-lg leading-loose">
-            <span className="text-red-500 font-bold">ReactNodeSend</span> allows
-            you to share files with end-to-end encryption and a file is deleted
-            after it is downloaded. So you can keep what you share private and
-            make sure your stuff does not stay online forever.
+      {url ? (
+        <>
+          <p className="text-center text-2xl">
+            <span className="font-bold text-red-700 text-3xl uppercase">
+              Your URL is:
+            </span>{" "}
+            {`${process.env.frontendURL}/links/${url}`}
           </p>
-          <Link href="/signup">
-            <p className="text-red-500 font-bold text-lg hover:text-red-700 hover:cursor-pointer">
-              Create an account for more benefits
-            </p>
-          </Link>
-        </div>
-      </div>
+          <button
+              type="button"
+              className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold hover:cursor-pointer mt-10"
+              onClick={() => navigator.clipboard.writeText(`${process.env.frontendURL}/links/${url}`)}
+            >Copy Link</button>
+        </>
+      ) : (
+        <>
+          {msg_file && <Alert message={msg_file} />}
+
+          <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+            <Dropzone />
+
+            <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+              <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
+                Simple and private file sharing
+              </h2>
+              <p className="text-lg leading-loose">
+                <span className="text-red-500 font-bold">ReactNodeSend</span>{" "}
+                allows you to share files with end-to-end encryption and a file
+                is deleted after it is downloaded. So you can keep what you
+                share private and make sure your stuff does not stay online
+                forever.
+              </p>
+              <Link href="/signup">
+                <p className="text-red-500 font-bold text-lg hover:text-red-700 hover:cursor-pointer">
+                  Create an account for more benefits
+                </p>
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
