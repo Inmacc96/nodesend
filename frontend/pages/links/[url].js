db70cb483b97acb2910bdca67a255dbf487152d8
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-15 21:56:33 +0100

    Show a form to enter the password in case the link has a password.

--- frontend/pages/links/[url].js
+++ frontend/pages/links/[url].js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import clientAxios from "../../config/axios";
 
 // Los props estáticos van a ser la respuesta que vamos a obtener.
@@ -44,20 +45,64 @@ export async function getSeverSidePaths() {
 }
 
 const Links = ({ link }) => {
-  console.log(link);
+  const [passwordAvailable, setPasswordAvailable] = useState(link.password);
+
+  const verifyPassword = (e) => {
+    e.preventDefault();
+
+  };
   return (
     <>
-      <h1 className="text-4xl text-center text-gray-700">
-        Download your file:
-      </h1>
-      <div className="flex items-center justify-center mt-10">
-        <a
-          href={`${process.env.backendURL}/api/files/${link.file}`}
-          className="bg-red-500 text-center px-10 py-3 rounded uppercase font-bold text-white cursor-pointer"
-        >
-          Here
-        </a>
-      </div>
+      {passwordAvailable ? (
+        <>
+          <p className="text-center">
+            This link is protected by a password, please enter it below
+          </p>
+          <div className="flex justify-center mt-5">
+            <div className="w-full max-w-lg">
+              <form
+                className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+                onSubmit={verifyPassword}
+              >
+                <div className="mb-4">
+                  <label
+                    htmlFor="password"
+                    className="block text-black text-sm font-bold mb-2"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="passowrd"
+                    placeholder="Link Password"
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold hover:cursor-pointer mt-3"
+                  value="Validate password"
+                />
+              </form>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="text-4xl text-center text-gray-700">
+            Download your file:
+          </h1>
+          <div className="flex items-center justify-center mt-10">
+            <a
+              href={`${process.env.backendURL}/api/files/${link.file}`}
+              className="bg-red-500 text-center px-10 py-3 rounded uppercase font-bold text-white cursor-pointer"
+            >
+              Here
+            </a>
+          </div>
+        </>
+      )}
     </>
   );
 };
